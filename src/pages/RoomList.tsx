bcea565921046add9c998ef53cf515de89972c85
yasuaki640 <yasuaki640@gmail.com>
2023-12-14 23:02:18 +0900

    ざせつしないか心配だぜえええ

---
+++ src/pages/RoomList.tsx
@@ -0,0 +1,5 @@
+import { type FC } from "react";
+
+export const RoomList: FC = () => {
+  return <h1>RoomList</h1>;
+};
---
+++ src/router/Router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import { RoomList } from "../pages/RoomList.tsx";
+import { Chat } from "../pages/Chat.tsx";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RoomList />,
+  },
+  {
+    path: "/chat/:id",
+    element: <Chat />,
+  },
+]);
