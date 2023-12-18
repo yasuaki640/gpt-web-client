fcf1ebb358e4c4f62afe5ecfd401e582cc14bafd
yasuaki640 <yasuaki640@gmail.com>
2023-12-18 20:28:04 +0900

    add open ai api client

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,32 @@
 import { Hono } from "hono";
 import { basicAuth } from "hono/basic-auth";
+import OpenAI from "openai";
+import { openaiMiddleware } from "./openai";
 
-type Bindings = {
+export type Bindings = {
 	USERNAME: string;
 	PASSWORD: string;
+	OPENAI_API_KEY: string;
 };
 
-const app = new Hono<{ Bindings: Bindings }>();
+export type Variables = {
+	openai: OpenAI;
+};
 
-app.use(async (c, next) => {
+const app = new Hono<{
+	Bindings: Bindings;
+	Variables: Variables;
+}>();
+
+app.use("*", async (c, next) => {
 	const auth = basicAuth({
 		username: c.env.USERNAME,
 		password: c.env.PASSWORD,
 	});
 	return auth(c, next);
 });
+
+app.use("*", openaiMiddleware);
 
 app.get("/", (c) =>
 	c.html(
@@ -25,5 +37,21 @@ app.get("/", (c) =>
 		</html>,
 	),
 );
+
+app.get("/chat", async (c) => {
+	const chatCompletion = await c.var.openai.chat.completions.create({
+		messages: [{ role: "user", content: "Say this is a test" }],
+		model: "gpt-3.5-turbo",
+	});
+
+	return c.html(
+		<html lang={"ja"}>
+			<body>
+				<textarea id={"chat"} />
+				<button type={"submit"}>Send</button>
+			</body>
+		</html>,
+	);
+});
 
 export default app;
---
+++ src/openai.ts
@@ -0,0 +1,14 @@
+import { MiddlewareHandler } from "hono";
+import OpenAI from "openai";
+import { type Bindings, type Variables } from "./index";
+
+export const openaiMiddleware: MiddlewareHandler<{
+	Bindings: Bindings;
+	Variables: Variables;
+}> = async (c, next) => {
+	const client = new OpenAI({
+		apiKey: c.env.OPENAI_API_KEY,
+	});
+	c.set("openai", client);
+	await next();
+};
