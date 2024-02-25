d829f9194013c1552dc794f13dc13dc10f4843d5
yasuaki640 <yasuaki640@gmail.com>
2024-02-25 20:25:04 +0900

    Add trigger to update roomUpdated timestamp when a new message is inserted

--- drizzle/0001_define_messages.sql
+++ drizzle/0001_define_messages.sql
@@ -5,3 +5,12 @@ CREATE TABLE `messages` (
 	`message` text NOT NULL,
 	`messageCreated` text DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+-- added by me
+CREATE TRIGGER IF NOT EXISTS messages_updated
+    AFTER INSERT ON messages
+BEGIN
+    UPDATE rooms
+    SET roomUpdated = CURRENT_TIMESTAMP
+    WHERE roomId = NEW.roomId;
+END;
