5265ca895a8d7760b8412865dc4e47225c33aecb
yasuaki640 <yasuaki640@gmail.com>
2024-07-14 00:43:38 +0900

    Remove unnecessary script tag for Go language highlighting in Layout.tsx file

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -8,10 +8,8 @@ export const Layout: FC = ({ children }) => (
         href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css"
       />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js" />
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/go.min.js" />
       <script>hljs.highlightAll();</script>
     </head>
-
     <body>{children}</body>
   </html>
 );
