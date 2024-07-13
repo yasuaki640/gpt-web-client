e9e22969ece41a1a07bb675a16be16bbbab67e11
yasuaki640 <yasuaki640@gmail.com>
2024-07-14 00:40:01 +0900

    Added syntax highlighting for code blocks using highlight.js library in Layout component.

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -2,6 +2,16 @@ import type { FC } from "hono/jsx";
 
 export const Layout: FC = ({ children }) => (
   <html lang={"ja"}>
+    <head>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css"
+      />
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/go.min.js"></script>
+      <script>hljs.highlightAll();</script>
+    </head>
+
     <body>{children}</body>
   </html>
 );
