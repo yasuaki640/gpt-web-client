2354d3f93a63f80c192b062f1b41e5a2099a7178
yasuaki640 <yasuaki640@gmail.com>
2024-07-27 23:13:07 +0900

    Add title element to Layout component for setting page title

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -9,6 +9,7 @@ export const Layout: FC = ({ children }) => (
       />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js" />
       <script>hljs.highlightAll();</script>
+      <title>gpt-web-client</title>
     </head>
     <body>{children}</body>
   </html>
